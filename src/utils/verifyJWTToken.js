655a24b3f7cf39d2433b8c932b098410474ef395
Denis Potapov <darsa.usalo@aurigagames.com>
2021-05-05 01:13:16 +0300

    Update jwt logging

--- src/utils/verifyJWTToken.js
+++ src/utils/verifyJWTToken.js
@@ -6,6 +6,7 @@ module.exports = (token) => new Promise((resolve, reject) => {
         process.env.JWT_SECRET || "",
         (err, decodedData) => {
             if (err || !decodedData) {
+                console.log("failed to verify JWT:", process.env.JWT_SECRET, ", token:", token)
                 return reject(err);
             }
 
