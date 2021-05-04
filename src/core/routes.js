73f096cbbb79c90de4abc997fb792e39436cc844
Denis Potapov <darsa.usalo@aurigagames.com>
2021-05-05 00:46:33 +0300

    Update cors

--- src/core/routes.js
+++ src/core/routes.js
@@ -14,7 +14,12 @@ const createRoutes = (app, io) => {
     const meetingController = new MeetingController(io);
 
     app.use(logger("dev"));
-    app.use(cors());
+    app.use(cors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }));
     app.use(bodyParser.json());
     app.use(checkAuth);
 
